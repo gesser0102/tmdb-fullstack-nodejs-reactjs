15b9cc6aa3dc69401839ff9fac556c380dd94c3a
Rodrigo Gesser <gesserrodrigo@hotmail.com>
2024-09-14 18:49:54 -0300

    fix: user routes

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,115 +1,90 @@
+import express from "express";
+import { body } from "express-validator";
+import favoriteController from "../controllers/favoriteControlle.js";
+import userController from "../controllers/userController.js";
+import requestHandler from "../handlers/requestHandler.js";
 import userModel from "../models/userModel.js";
-import jsonwebtoken from "jsonwebtoken";
-import responseHandler from "../handlers/responseHandler.js";
-import crypto from 'crypto';
+import token from "../middlewares/token.js";
 
-const signup = async (req, res) => {
-  try {
-    console.log("Request body:", req.body);  // Log input data
-    const { username, password} = req.body;
+const router = express.Router();
 
-    const checkUser = await userModel.findOne({ username });
-    console.log("Check user exists:", checkUser);  // Log if user already exists
+router.post(
+  "/signup",
+  body("username")
+    .exists().withMessage("Usuário é obrigat¢rio")
+    .custom(async value => {
+      const user = await userModel.findOne({ username: value });
+      if (user) return Promise.reject("Usuario já existe");
+    }),
+  body("password")
+    .exists().withMessage("Senha é obrigat¢ria"),
+  body("confirmPassword")
+    .exists().withMessage("Confirmar senha é obrigat¢ria")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) throw new Error("Senhas não conferem");
+      return true;
+    }),
+    requestHandler.validate,
+    userController.signup
+);
 
-    if (checkUser) return responseHandler.badrequest(res, "username already used");
-
-    const user = new userModel();
-    user.username = username;
-    user.setPassword(password);
-
-    await user.save();
-    console.log("User saved:", user); 
-
-    const token = jsonwebtoken.sign(
-      { data: user.id },
-      process.env.TOKEN_SECRET,
-      { expiresIn: "24h" }
-    );
-    console.log("JWT Token:", token); 
-
-    responseHandler.created(res, {
-      token,
-      ...user._doc,
-      id: user.id
-    });
-  } catch (error) {
-    console.error("Signup error:", error);  
-    responseHandler.error(res);
-  }
-};
+router.post(
+  "/signin",
+  body("username")
+    .exists().withMessage("Usuário é obrigatório"),
+  body("password")
+    .exists().withMessage("Senha é obrigatória"),
+  requestHandler.validate,
+  userController.signin
+);
 
 
-const signin = async (req, res) => {
-  try {
-    const { username, password } = req.body;
+router.get(
+  "/info",
+  token.auth,
+  userController.getInfo
+);
 
-    const user = await userModel.findOne({ username }).select("username password salt id");
+router.get(
+  "/favorites",
+  token.auth,
+  favoriteController.getFavorites
+);
 
-    if (!user) return responseHandler.badrequest(res, "Usuário não existe!");
+router.post(
+  "/favorites",
+  token.auth,
+  body("mediaType")
+    .exists().withMessage("mediaType é obrigatório")
+    .custom(type => ["movie", "tv"].includes(type)).withMessage("mediaType invalida"),
+  body("mediaId")
+    .exists().withMessage("mediaId is required")
+    .isLength({ min: 1 }).withMessage("mediaId não pode ser vazio"),
+  body("mediaTitle")
+    .exists().withMessage("mediaTitle é obrigatório"),
+  body("mediaPoster")
+    .exists().withMessage("mediaPoster é obrigatório"),
+  body("mediaRate")
+    .exists().withMessage("mediaRate é obrigatório"),
+  requestHandler.validate,
+  favoriteController.addFavorite
+);
 
-    if (!user.validPassword(password)) return responseHandler.badrequest(res, "Senha Inválida!");
+router.delete(
+  "/favorites/:favoriteId",
+  token.auth,
+  favoriteController.removeFavorite
+);
 
-    const token = jsonwebtoken.sign(
-      { data: user.id },
-      process.env.TOKEN_SECRET,
-      { expiresIn: "24h" }
-    );
+router.post(
+  "/generate-share-link",
+  token.auth,
+  userController.generateShareToken
+),
 
-    user.password = undefined;
-    user.salt = undefined;
+router.get(
+  "/favorites/share/:shareToken",
+  favoriteController.getSharedFavorites
+);
 
-    responseHandler.created(res, {
-      token,
-      ...user._doc,
-      id: user.id
-    });
-  } catch {
-    responseHandler.error(res);
-  }
-};
-
-const getInfo = async (req, res) => {
-  try {
-    const user = await userModel.findById(req.user.id);
-
-    if (!user) return responseHandler.notfound(res);
-
-    responseHandler.ok(res, user);
-  } catch {
-    responseHandler.error(res);
-  }
-};
-
-const generateShareToken = async (req, res) => {
-  try {
-    console.log("Iniciando generateShareToken");
-
-    // Verifique se req.user está definido
-    if (!req.user || !req.user.id) {
-      console.error("Usuário não autenticado");
-      return responseHandler.unauthorized(res);
-    }
-
-    const userId = req.user.id;
-
-    // Gera um token aleatório
-    const shareToken = crypto.randomBytes(16).toString('hex');
-
-    // Atualiza o usuário com o novo token
-    const updatedUser = await userModel.findByIdAndUpdate(userId, { shareToken }, { new: true });
-
-    console.log("Usuário atualizado:", updatedUser);
-
-    responseHandler.ok(res, { shareToken });
-  } catch (error) {
-    console.error("Erro em generateShareToken:", error);
-    responseHandler.error(res);
-  }
-};
-
-export default {
-  signup,
-  signin,
-  getInfo,
-  generateShareToken
-};
+export default router;
